b5bfb492542f4f0b514a590a79e17252840fbd93
N8866U <willfuld04@gmail.com>
2023-10-25 17:17:54 -0400

    Changes the main.py to encoder.py and adds decoder.py, both files contain an empty function

---
+++ decoder.py
@@ -0,0 +1,11 @@
+# The objective of this lab is to understand the purpose and usefulness of
+# version control. You will work in a group of two. You will write a simplified
+# password encoder/decoder program and practice the functions of version
+# control systems to develop familiarity with them.
+
+
+def main():
+    pass
+
+
+main()
