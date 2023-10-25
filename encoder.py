f513f9b0f7ad958bf9561b48968ffd0db99a2b38
N8866U <willfuld04@gmail.com>
2023-10-25 18:07:48 -0400

    Reworks logic to contain the required menu and uses function for encode and decode

--- encoder.py
+++ encoder.py
@@ -1,19 +1,43 @@
-# The password encoder should take in an 8-digit password in string
-# format containing only integers. After passing the password into
-# the encoder, the encoder stores the encoded password to a new
-# variable, with each digit being shifted up by 3 numbers.
+# The objective of this lab is to understand the purpose and usefulness of
+# version control. You will work in a group of two. You will write a
+# simplified password encoder/decoder program and practice the
+# functions of version control systems to develop familiarity with them.
 
 
-def main():
-    password = input("Enter password: ")
+def encode():
+    password = input("Please enter your password to encode: ")
+    result = ""
     for i in range(0, len(password)):
         num = int(password[i])
         num += 3
         if num >= 10:
             num -= 10
-            print(f"{num}", end="")
+            result = result + str(num)
         else:
-            print(f"{num}", end="")
+            result = result + str(num)
+        return result
+
+
+def decode():
+    pass
+
+
+def main():
+    while True:
+        print(f"\nMenu \n-------------")
+        print(f"1. Encode \n2. Decode \n3. Quit\n")
+        option = int(input("Please enter an option: "))
+        encoded = ""
+        if option == 1:
+            encoded = encode()
+            print("Your password has been encoded and stored!")
+        elif option == 2:
+            decoded = decode()
+            print(f"The encoded password is {encoded}, and the original password is {decoded}.")
+        elif option == 3:
+            quit()
+        else:
+            print("Error, try again")
 
 
 if __name__ == '__main__':
